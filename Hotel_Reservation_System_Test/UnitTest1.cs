71df14b53ccee67f6198b246329abb70d0827673
VSRUJANA <srujana.valavala@gmail.com>
2020-11-03 22:09:38 +0530

    [srujana] Add. Testcases TC-2.1,2.2,2.3 to test FindCheapestHotel method

--- Hotel_Reservation_System_Test/UnitTest1.cs
+++ Hotel_Reservation_System_Test/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Hotel_Reservation_System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Hotel_Reservation_System_Test
@@ -19,6 +21,52 @@ namespace Hotel_Reservation_System_Test
             int actual = hotelManager.hotels.Count();
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GivenInvalidDateRange_IsDateRangeValid_ShouldReturnFalse()
+        {
+            DateTime start = DateTime.Parse("17Nov2020");
+            DateTime end = DateTime.Parse("12Nov2020");
+            bool expected = false; 
+
+            ManageHotels hotelManager = new ManageHotels();
+            bool result = hotelManager.IsDateRangeValid(start, end);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GivenDateRange_FindCheapestHotelInAGivenDateRangeMethod_ShouldReturnCheapestHotel()
+        {
+            DateTime start = DateTime.Parse("12Nov2020");
+            DateTime end = DateTime.Parse("14Nov2020");
+            string expected = "Lakewood";
+
+            ManageHotels hotelManager = new ManageHotels();
+            hotelManager.AddHotel(new Hotel("Lakewood", 110));
+            hotelManager.AddHotel(new Hotel("Bridgewood", 160));
+            hotelManager.AddHotel(new Hotel("Ridgewood", 220));
+            List<Hotel> cheapestHotels = hotelManager.FindCheapestHotelInAGivenDateRange(start, end);
+
+            Assert.AreEqual(expected, cheapestHotels[0].name);
+        }
+
+        [TestMethod]
+        public void GivenDateRange_FindCheapestHotelInAGivenDateRangeMethod_ShouldReturnAllAvailableCheapestHotels()
+        {
+            DateTime start = DateTime.Parse("12Nov2020");
+            DateTime end = DateTime.Parse("14Nov2020");
+            string[] expected = { "Lakewood", "Ridgewood" };
+
+            ManageHotels hotelManager = new ManageHotels();
+            hotelManager.AddHotel(new Hotel("Lakewood", 110));
+            hotelManager.AddHotel(new Hotel("Bridgewood", 160));
+            hotelManager.AddHotel(new Hotel("Ridgewood", 110));
+            List<Hotel> cheapestHotelsList = hotelManager.FindCheapestHotelInAGivenDateRange(start, end);
+            string[] cheapestHotelsArray = cheapestHotelsList.Select(hotel => hotel.name).ToArray();
+
+            CollectionAssert.AreEqual(expected, cheapestHotelsArray);
         }
     }
 }
